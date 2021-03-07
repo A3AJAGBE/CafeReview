989d0627f7ca171177c6a3edcd11250fd685fc40
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-07 07:21:42 +0000

    Add more fields to form

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import os
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, url
 from dotenv import load_dotenv
 load_dotenv()
 
 
 class CafeForm(FlaskForm):
+    beverage = SelectField('Beverage', choices=['Tea 🫖', 'Coffee ☕', 'Chocolate 🧋️️'])
     cafe = StringField('Cafe name', validators=[DataRequired()])
+    location = StringField('Location', validators=[DataRequired(), url()])
+    rating = SelectField('Beverage Rating', choices=['★️', '★️★️️', '★️★️★️️', '★️★️★️★️', '★️★️★️★️★️️'])
+    wifi = SelectField('Wifi Rating', choices=['👍🏼', '👍🏼👍🏼', '👍🏼👍🏼👍🏼', '👍🏼👍🏼👍🏼👍🏼', '👍🏼👍🏼👍🏼👍🏼👍🏼'])
     submit = SubmitField('Submit')
 
 
@@ -33,9 +37,13 @@ def cafe():
 def add():
     form = CafeForm()
     if form.validate_on_submit():
+        beverage = form.beverage.data
         cafe_name = form.cafe.data
+        location = form.location.data
+        rating = form.rating.data
+        wifi = form.wifi.data
         with open('cafe-data.csv', mode='a') as csv_file:
-            csv_file.write(f'{cafe_name}\n')
+            csv_file.write(f'{beverage},{cafe_name},{location},{rating},{wifi}\n')
             return redirect(url_for('index'))
     return render_template('add.html', form=form)
 
