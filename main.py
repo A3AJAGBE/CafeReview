b9c3b88cadd115bb76d5a982e0ad9d6868bb6820
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-07 06:44:25 +0000

    Add form class

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from dotenv import load_dotenv
 load_dotenv()
+
+
+class CafeForm(FlaskForm):
+    cafe = StringField('Cafe name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -20,9 +29,15 @@ def cafe():
     return render_template('cafe.html')
 
 
-@app.route('/add')
+@app.route('/add', methods=('GET', 'POST'))
 def add():
-    return render_template('add.html')
+    form = CafeForm()
+    if form.validate_on_submit():
+        cafe_name = form.cafe.data
+        with open('cafe-data.csv', mode='a') as csv_file:
+            csv_file.write(f'{cafe_name}\n')
+            return redirect(url_for('index'))
+    return render_template('add.html', form=form)
 
 
 if __name__ == '__main__':
