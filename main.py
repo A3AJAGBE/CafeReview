208f05d8c48e165523476732869e5af0513f3145
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-07 07:51:01 +0000

    Add list of cafes to cafe function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -10,9 +10,9 @@ load_dotenv()
 
 
 class CafeForm(FlaskForm):
+    cafe = StringField('Cafe', validators=[DataRequired()])
     beverage = SelectField('Beverage', choices=['Tea 🫖', 'Coffee ☕', 'Chocolate 🧋️️'])
-    cafe = StringField('Cafe name', validators=[DataRequired()])
-    location = StringField('Location', validators=[DataRequired(), url()])
+    location = StringField('Google Maps Location', validators=[DataRequired(), url()])
     rating = SelectField('Beverage Rating', choices=['★️', '★️★️️', '★️★️★️️', '★️★️★️★️', '★️★️★️★️★️️'])
     wifi = SelectField('Wifi Rating', choices=['👍🏼', '👍🏼👍🏼', '👍🏼👍🏼👍🏼', '👍🏼👍🏼👍🏼👍🏼', '👍🏼👍🏼👍🏼👍🏼👍🏼'])
     submit = SubmitField('Submit')
@@ -30,21 +30,26 @@ def index():
 
 @app.route('/cafe')
 def cafe():
-    return render_template('cafe.html')
+    with open('cafe-data.csv', newline='') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('cafe.html', cafes=list_of_rows)
 
 
 @app.route('/add', methods=('GET', 'POST'))
 def add():
     form = CafeForm()
     if form.validate_on_submit():
-        beverage = form.beverage.data
         cafe_name = form.cafe.data
+        beverage = form.beverage.data
         location = form.location.data
         rating = form.rating.data
         wifi = form.wifi.data
         with open('cafe-data.csv', mode='a') as csv_file:
-            csv_file.write(f'{beverage},{cafe_name},{location},{rating},{wifi}\n')
-            return redirect(url_for('index'))
+            csv_file.write(f'{cafe_name}, {beverage}, {location}, {rating}, {wifi}\n')
+            return redirect(url_for('cafe'))
     return render_template('add.html', form=form)
 
 
